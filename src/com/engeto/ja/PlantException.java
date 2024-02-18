03c8efdfa68a3decd64e547463afb77520a79c20
Zuzana Tománková <Tomankova.Z@seznam.cz>
2024-02-18 11:10:36 +0100

    úprava třídy PlantException

--- src/com/engeto/ja/PlantException.java
+++ src/com/engeto/ja/PlantException.java
@@ -3,17 +3,14 @@ package com.engeto.ja;
 import java.time.LocalDate;
 
 public class PlantException extends Exception{
-    public void setFrequencyOfWatering (int frequencyOfWatering){
-       if (frequencyOfWatering<=0){
-           throw new IllegalArgumentException("Frequency of watering must be greater than zero.Given frequency: "+frequencyOfWatering);
-       }
-    }
-    public class WateringException extends Exception{
-        public void setWatering   (LocalDate planted, LocalDate watering){
-        if (watering.isBefore(planted)){
-            throw new IllegalArgumentException("Watering must be after planting. Given watering: "+watering+" Given planting: "+planted);
-        }}
-    }
+    public PlantException(String message) {
+        super(message);
+    }}
 
 
-}
+
+
+
+
+
+
