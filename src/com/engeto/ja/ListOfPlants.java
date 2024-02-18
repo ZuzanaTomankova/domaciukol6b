a954b85c27697ba21c732a5acc36946d58c4894b
Zuzana Tománková <Tomankova.Z@seznam.cz>
2024-02-18 01:11:52 +0100

    vytvoření metody saveContentToFile

--- src/com/engeto/ja/ListOfPlants.java
+++ src/com/engeto/ja/ListOfPlants.java
@@ -1,11 +1,7 @@
 package com.engeto.ja;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.math.BigDecimal;
+import java.io.*;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 import java.util.List;
@@ -17,9 +13,11 @@ public class ListOfPlants {
     public void addPlant(Plant plant) {
         plantList.add(plant);
     }
+
     public Plant plant(int index) {
         return plantList.get(index);
     }
+
     public void removePlant(int index) {
         plantList.remove(index);
     }
@@ -33,8 +31,9 @@ public class ListOfPlants {
     }
 
     private List<Plant> plants = new ArrayList<>();
+
     public void loadContentFromFile(String fileName) throws RuntimeException {
-        fileName="ressources/kvetiny.txt";
+
         int lineCounter = 0;
         plants.clear();
         try (Scanner scanner = new Scanner(new BufferedReader(new FileReader(fileName)))) {
@@ -43,31 +42,67 @@ public class ListOfPlants {
                 String line = scanner.nextLine();
                 String[] parts = line.split("\t");
                 if (parts.length != 5) throw new RuntimeException(
-                        "Nesprávný počet položek na řádku číslo: "+lineCounter+": "+line+"!");
+                        "Nesprávný počet položek na řádku číslo: " + lineCounter + ": " + line + "!");
                 String name = parts[0];
                 String description = parts[1];
                 LocalDate planted = LocalDate.parse(parts[2]);
                 LocalDate watering = LocalDate.parse(parts[3]);
                 int frequencyOfWatering = Integer.parseInt(parts[4]);
-                Plant plant = new Plant(name,description,planted,watering,frequencyOfWatering);
-                plants.add(plant);
-                            }
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException("Soubor "+fileName+" nebyl nalezen!\n"+ e.getLocalizedMessage());
-        } catch (NumberFormatException e) {
-            throw new RuntimeException("Chyba při čtení číselné hodnoty na řádku číslo: "+lineCounter+":\n"
-                    + e.getLocalizedMessage());
-        } catch (IllegalArgumentException e) {
-            throw new RuntimeException("Chyba při čtení kategorie na řádku číslo: "+lineCounter+":\n"
-                    + e.getLocalizedMessage());
-        } catch (DateTimeParseException e) {
-            throw new RuntimeException("Chyba při čtení data na řádku číslo: "+lineCounter+":\n"
-                    + e.getLocalizedMessage());
-        }
+                Plant plant1 = new Plant(name, description, planted, watering, frequencyOfWatering);
+                plants.add(plant1);
+
+            }
+
+
+    } catch(
+    FileNotFoundException e)
+
+    {
+        throw new RuntimeException("Soubor " + fileName + " nebyl nalezen!\n" + e.getLocalizedMessage());
+    } catch(
+    NumberFormatException e)
+
+    {
+        throw new RuntimeException("Chyba při čtení číselné hodnoty na řádku číslo: " + lineCounter + ":\n"
+                + e.getLocalizedMessage());
+    } catch(
+    IllegalArgumentException e)
+
+    {
+        throw new RuntimeException("Chyba při čtení kategorie na řádku číslo: " + lineCounter + ":\n"
+                + e.getLocalizedMessage());
+    } catch(
+    DateTimeParseException e)
+
+    {
+        throw new RuntimeException("Chyba při čtení data na řádku číslo: " + lineCounter + ":\n"
+                + e.getLocalizedMessage());
     }
 
 
+  for(Plant plant1 :plants) { System.out.println(plant1.getName() + plant1.getWatering());}
 
 
+}
+
+
+    public void saveContentToFile(String fileName) throws RuntimeException {
+        String delimiter = "\t";
+        try (PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(fileName)))) {
+            for (Plant plant1 : plants) {
+                writer.println(plant1.getName() + "\t"
+                        + plant1.getNotes() + "\t"
+                        + plant1.getPlanted() + "\t"
+                        + plant1.getWatering() + "\t"
+                        + plant1.getFrequencyOfWatering() + "\t");
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("Soubor "+fileName+" nebyl nalezen!\n"
+                    + e.getLocalizedMessage());
+        } catch (IOException e) {
+            throw new RuntimeException("Chyba výstupu při zápisu do souboru: "+fileName
+                    +":\n"+ e.getLocalizedMessage());
+        }
+    }
 
 }
